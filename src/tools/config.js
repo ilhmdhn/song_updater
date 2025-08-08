9a6b63fed28767f41a29117312793f504f6d67d4
Ilham Dohaan <doha39anilham@gmail.com>
2025-08-08 08:45:45 +0700

    aaaa

--- src/tools/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    folderTemp: 'C:\\song_update_temp',
-    mainServerIp: '\\\\192.168.1.14',
-    backupServerIp: '\\\\192.168.1.12',
-    baseUrl: 'https://eportal.happypuppy.id/Api'
-}
